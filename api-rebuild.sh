28cddc4a045ab3e749eeb3c81fdcf5ac93cb1f11
flaminestone <flamine@list.ru>
2018-03-28 17:57:45 +0300

    Delete unused scripts, configs anf args from docker-compose

--- api-rebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker-compose build --no-cache api-server
-docker-compose down api-server
-docker-compose up api-server
--- rebuild-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-docker-compose down clientside-server
-docker-compose build clientside-server
-docker-compose up clientside-server
-docker-compose down clientside-server
-docker-compose build clientside-server
-docker-compose up clientside-server
