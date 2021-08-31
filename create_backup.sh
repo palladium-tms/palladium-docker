17dba1121d1262f021ad2c8fe7b808e5001cfe9c
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-08-31 16:41:36 +0300

    Major cleanup of backup script (#3)
    1. Fix all issues via `shellcheck`
    2. Get name of container dynamically
    3. Cleanup comments

--- create_backup.sh
+++ create_backup.sh
@@ -1,24 +1,19 @@
-# apt install awscli
-# aws configure
+#!/bin/bash
 
-DB_USER='postgres'
-DB_PASS=''
-DB_BASE='postgres'
-CONTAINER_NAME='palladium-docker_db_1_2ee01774bd29'
+# Correctly configure aws cli before running this script
+# `aws configure`
 
+DB_CONTAINER_ID=$(docker-compose ps -q db)
 
-DATE=`date '+%Y-%m-%d-%H-%M-%S'`
+DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 BACKUP_NAME=palladium-backup-$DATE.tar.gz
 echo "Dump database"
-docker exec -it $CONTAINER_NAME bash -c 'pg_dump -U postgres postgres > /tmp/outfile'
+docker exec -it "$DB_CONTAINER_ID" bash -c 'pg_dump -U postgres postgres > /tmp/outfile'
 echo "Copy backup to host"
-docker cp $CONTAINER_NAME:/tmp/outfile $BACKUP_NAME
-echo 'Deleteing from container'
-docker exec -it $CONTAINER_NAME bash -c 'rm /tmp/outfile'
+docker cp "$DB_CONTAINER_ID":/tmp/outfile "$BACKUP_NAME"
+echo 'Deleting from container'
+docker exec -it "$DB_CONTAINER_ID" bash -c 'rm /tmp/outfile'
 echo 'Loading to s3'
-aws s3 cp $BACKUP_NAME s3://palladium-backup/$BACKUP_NAME
+aws s3 cp "$BACKUP_NAME" s3://palladium-backup/"$BACKUP_NAME"
 echo 'Removing from host'
-rm $BACKUP_NAME
-
-
-
+rm "$BACKUP_NAME"
