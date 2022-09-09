236433faab8e3cb100e52fb92d4c522f5f65604a
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-09 18:16:42 +0300

    Fix stuff

--- restore_backup.sh
+++ restore_backup.sh
@@ -10,5 +10,5 @@ BACKUP_NAME=''
 aws s3 cp s3://palladium-backup/"$BACKUP_NAME" .
 docker cp "$BACKUP_NAME" "$DB_CONTAINER_ID":/tmp/
 docker exec "$DB_CONTAINER_ID" pg_restore -j $CPU_COUNT -U postgres -d postgres /tmp/"$BACKUP_NAME"
-docker exec "$DB_CONTAINER_ID" rm /tmp/$BACKUP_NAME
+docker exec "$DB_CONTAINER_ID" rm /tmp/"$BACKUP_NAME"
 rm "$BACKUP_NAME"
