9dfbd6c45907447a9d20c1ebabd1a58aa06b79e4
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-09-09 18:21:32 +0300

    Use `pg_dump`\`pg_restore` combination for making backup (#17)
    * Use `pg_dump`\`pg_restore` combination for making backup
    
    * Fix more
    
    * Fix restore backuup more
    
    * Remove backup name
    
    * Fix stuff

--- restore_backup.sh
+++ restore_backup.sh
@@ -3,8 +3,12 @@
 # Correctly configure aws cli before running this script
 # `aws configure`
 
+CPU_COUNT=4
+DB_CONTAINER_ID=$(docker-compose ps -q db)
 BACKUP_NAME=''
 
 aws s3 cp s3://palladium-backup/"$BACKUP_NAME" .
-zcat "$BACKUP_NAME" | docker exec -i palladium-docker_db_1 psql -U postgres
+docker cp "$BACKUP_NAME" "$DB_CONTAINER_ID":/tmp/
+docker exec "$DB_CONTAINER_ID" pg_restore -j $CPU_COUNT -U postgres -d postgres /tmp/"$BACKUP_NAME"
+docker exec "$DB_CONTAINER_ID" rm /tmp/"$BACKUP_NAME"
 rm "$BACKUP_NAME"
