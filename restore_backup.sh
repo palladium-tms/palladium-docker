192d371b54f30a034d783ffc453ce83495b04e80
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-09 17:52:08 +0300

    Fix restore backuup more

--- restore_backup.sh
+++ restore_backup.sh
@@ -3,10 +3,12 @@
 # Correctly configure aws cli before running this script
 # `aws configure`
 
-BACKUP_NAME=''
+CPU_COUNT=4
+DB_CONTAINER_ID=$(docker-compose ps -q db)
+BACKUP_NAME='palladium-backup-2022-09-09-13-25-52.pg_dump'
 
 aws s3 cp s3://palladium-backup/"$BACKUP_NAME" .
-docker cp "$BACKUP_NAME" palladium-docker-db-1:/tmp/
-docker exec -i palladium-docker-db-1 pg_restore -U postgres -f /tmp/"$BACKUP_NAME"
-docker exec palladium-docker-db-1 'rm /tmp/$BACKUP_NAME'
+docker cp "$BACKUP_NAME" "$DB_CONTAINER_ID":/tmp/
+docker exec "$DB_CONTAINER_ID" pg_restore -j $CPU_COUNT -U postgres -d postgres /tmp/"$BACKUP_NAME"
+docker exec "$DB_CONTAINER_ID" rm /tmp/$BACKUP_NAME
 rm "$BACKUP_NAME"
